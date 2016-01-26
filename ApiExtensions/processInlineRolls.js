63acf961ae6d236f79860682b025f766bf4f61f8
Jeff Bennett <jfflbnntt@concentricsky.com>
2016-01-25 16:11:44 -0800

    convert tabs to spaces

--- ApiExtensions/processInlineRolls.js
+++ ApiExtensions/processInlineRolls.js
@@ -1,25 +1,25 @@
 // general function for processing inlien rolls and tables
 function processInlineRolls(msg) {
-	if(_.has(msg,'inlinerolls')){
-		return _.chain(msg.inlinerolls)
-		.reduce(function(m,v,k){
-			var ti=_.reduce(v.results.rolls,function(m2,v2){
-				if(_.has(v2,'table')){
-					m2.push(_.reduce(v2.results,function(m3,v3){
-						m3.push(v3.tableItem.name);
-						return m3;
-					},[]).join(', '));
-				}
-				return m2;
-			},[]).join(', ');
-			m['$[['+k+']]']= (ti.length && ti) || v.results.total || 0;
-			return m;
-		},{})
-		.reduce(function(m,v,k){
-			return m.replace(k,v);
-		},msg.content)
-		.value();
-	} else {
-		return msg.content;
-	}
+    if(_.has(msg,'inlinerolls')){
+        return _.chain(msg.inlinerolls)
+        .reduce(function(m,v,k){
+            var ti=_.reduce(v.results.rolls,function(m2,v2){
+                if(_.has(v2,'table')){
+                    m2.push(_.reduce(v2.results,function(m3,v3){
+                        m3.push(v3.tableItem.name);
+                        return m3;
+                    },[]).join(', '));
+                }
+                return m2;
+            },[]).join(', ');
+            m['$[['+k+']]']= (ti.length && ti) || v.results.total || 0;
+            return m;
+        },{})
+        .reduce(function(m,v,k){
+            return m.replace(k,v);
+        },msg.content)
+        .value();
+    } else {
+        return msg.content;
+    }
 }
